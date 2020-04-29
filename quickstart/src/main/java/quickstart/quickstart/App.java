5ff240c61a20dfc0caaeda582f2791e6c8b01f28
USER <appuprakash82>
2020-04-29 15:49:00 +0000

    ok

--- quickstart/src/main/java/quickstart/quickstart/App.java
+++ quickstart/src/main/java/quickstart/quickstart/App.java
@@ -8,6 +8,15 @@ public class App
 {
     public static void main( String[] args )
     {
+    	int b=a(20);
+    	System.out.println(b);
         System.out.println( "Hello World!" );
+    }
+    
+    public static int a(int a)
+    {
+    	a=10;
+    	return a;
+    	//System.out.println("Test");
     }
 }
